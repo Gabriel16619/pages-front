bbad9d8490c9032f290b92766ba73283dadff33b
GABRIEL CAVALCANTE DOS SANTOS <25132825@dsn127m1246733.cfp127aluno.local>
2025-08-04 15:20:29 -0300

    envio da troca de cores

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+//deixando ele mais rigoroso as regras
+//declarando uma variavel
+const buttonTrocarCor = document.getElementById('trocar-cor')
+//criacao de funcao
+function trocarCor() {
+    const cor = document.getElementById('cor').value
+   document.documentElement.style.setProperty('--bg-color', cor)
+}
+
+buttonTrocarCor.addEventListener('click', trocarCor)
